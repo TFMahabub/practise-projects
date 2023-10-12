25af2df8a66e9f4e22caa7824600ce4c21e8cba4
Mahabub Alam <rjmahabub543@gmail.com>
2023-10-12 21:47:46 +0600

    navbar structure make

---
+++ fiverr-clone/src/layouts/navbar/Index.tsx
@@ -0,0 +1,28 @@
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="nav-container">
+        <div className="right-side">
+          <div className="logo">
+            <img src="" alt="" />
+          </div>
+          <ul>
+            <li>Dashboard</li>
+            <li>Dashboard</li>
+            <li>Dashboard</li>
+            <li>Dashboard</li>
+          </ul>
+        </div>
+        <div className="left-side">
+          <div className="notification"></div>
+          <div className="message"></div>
+          <div className="help"></div>
+          <div className="profile-image"></div>
+          <div className="personal-usd"></div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
